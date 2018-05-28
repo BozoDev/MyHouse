5c70c7330dd674ffe49e5b5ce8be83e75d917d56
Bozo <keithwindev@gmail.com>
2018-05-28 14:55:45 +0200

    Added some rain-processing logic

--- tools/water/usr/local/bin/water.sh
+++ tools/water/usr/local/bin/water.sh
@@ -1,5 +1,10 @@
 #/bin/bash
-
+###
+# Calcs:
+#  Ter-Spr: ca. 20m^2 - 1" high uses 500l at 5.8l/min takes 86min per week
+#  Shed: ca. 20m^2 - 1" high uses 500l at 7l/min takes 71minutes
+#  Ship: ca. 6m^2 - 1" high uses 144l at 6.9l/min takes 20min
+#  Gara: ca 12m^2 - 1" high uses unknown
 ON=0
 OFF=1
 duration=1200
@@ -11,19 +16,39 @@ wateringfile="/tmp/watering.tmp"
 _mqtt_topic_caretaker="caretaker/gardener"
 _mqtt_broker="pi3gate"
 _mqtt_id="gardenpi-gardener"
-
+_mqtt_bin_pub="/usr/bin/mosquitto_pub"
+_mqtt_bin_sub="/usr/bin/mosquitto_sub"
 
 if [ "A$1" == "A-c" ]
 then
 	_amnt=$( /usr/local/bin/rain_calc.sh )
+        _skipWatering=0
+        _careMessage="Didn't water the garden, due to: "
 	if [ $_amnt -gt 300 ]
 	then
 		echo "Exiting due to rain > 30(mm * 10) the past X days..."
-		/usr/bin/mosquitto_pub -h "$_mqtt_broker" -i "$_mqtt_id" -t "$_mqtt_topic_caretaker" -m "Didn't water the garden, due to too much rain lately"
-		exit 2
-	else
-		shift
+		_careMessage="$_careMessage too much rain in the past 3 days"
+		let _skipWatering++
 	fi
+        _amnt=$( $_mqtt_bin_sub -h "$_mqtt_broker" -i "$_mqtt_id" -t "sensors/weather/hourRain_cm" )
+        if [ $_amnt -gt 30 ]
+        then
+          echo "Exiting due to rain past 1hr > 3(mm * 10)...."
+          _careMessage="$_careMessage too much rain in the past 1hr"
+          let _skipWatering++
+        fi
+        _amnt=$( $_mqtt_bin_sub -h "$_mqtt_broker" -i "$_mqtt_id" -t "sensors/weather/rain_cm" )
+        if [ $_amnt -gt 0 ]
+        then
+          echo "Exiting due to raining..."
+          _careMessage="$_careMessage it's $( date ) and currently raining"
+          let _skipWatering++
+          $_mqtt_bin_pub  -h "$_mqtt_broker" -i "$_mqtt_id" -t "$_mqtt_topic_caretaker" -m "Sleeping 12hrs and re-running"
+          sleep $(( 12*3600 ))
+          /usr/local/bin/water.sh ${@}
+        fi
+        [ $_skipWatering -gt 0 ] && $_mqtt_bin_pub  -h "$_mqtt_broker" -i "$_mqtt_id" -t "$_mqtt_topic_caretaker" -m "$_careMessage RainCondition counter: $_skipWatering" && exit 2
+        shift
 fi
 
 valve="$1"
